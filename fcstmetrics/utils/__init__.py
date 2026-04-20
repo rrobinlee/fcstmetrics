d2abbe45928a88a1b9d7238edb731051a717b2ad
rrobinlee <robleeusa@gmail.com>
2026-04-20 18:59:10 -0500

    Update __init__.py

--- fcstmetrics/utils/__init__.py
+++ fcstmetrics/utils/__init__.py
@@ -1,10 +1,11 @@
 from .sktime_utils import (convert_to_sktime_format, prepare_sktime_train_test, create_sktime_dataset_from_arrays)
-from .visualization import (plot_eda, plot_residuals, plot_predictions, plot_diagnostics)
+from .visualization import (plot_eda, plot_exog, plot_residuals, plot_predictions, plot_diagnostics)
 
 __all__ = ['convert_to_sktime_format',
            'prepare_sktime_train_test',
            'create_sktime_dataset_from_arrays',
            'plot_eda',
+           'plot_exog',
            'plot_residuals',
            'plot_predictions',
            'plot_diagnostics',
