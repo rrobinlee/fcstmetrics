a41354c2b9b08691ebab4cb46a5dbe58eb92b084
rrobinlee <robleeusa@gmail.com>
2026-04-19 23:02:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name="fcstmetrics",
       python_requires=">=3.7",
-      packages=find_packages(exclude=['examples', 'examples.*']),
+      packages=find_packages(exclude=['notebooks', 'notebooks.*']),
       install_requires=["numpy>=1.20.0", 
                         "pandas>=1.3.0", 
                         "scipy>=1.7.0", 
